153645215879c4fbfe21f9944d475c368212cced
Jakob Nanneson <jakob.nanneson@coop.se>
2020-01-27 16:47:09 +0100

    generate all products from response

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,22 +4,10 @@
       <div class="mdc-button__ripple"></div>
       <span class="mdc-button__label">Button: {{ msg }}</span>
     </button>
-
-    <p>{{ error }}</p>
-
-    <div v-for="product in productList" :key="product.code">
-      <pre>{{ product.name }} <template v-if="product.fromSweden">🇸🇪</template><strong>{{ product.price.formattedValue }} kr</strong></pre>
-    </div>
-    --
-    <pre>{{ productList }}</pre>
   </div>
 </template>
 
 <script lang="ts">
-import axios from "axios";
-import VueAxios from "vue-axios";
-Vue.use(VueAxios, axios);
-
 import Vue from "vue";
 import Component from "vue-class-component";
 
@@ -28,26 +16,7 @@ import Component from "vue-class-component";
     msg: String
   }
 })
-export default class HelloWorld extends Vue {
-  productList = null;
-  error = null;
-
-  mounted() {
-    axios
-      .get(
-        // "https://www.coop.se/api/hybris/ecommerce/product/recommendations?placements[]=generic_page.generic_recs1"
-        // "https://www.coop.se/api/hybris/ecommerce/product/recommendations?placements[]=home_page.horizontal_recs1"
-        "https://www.coop.se/ws/v2/coop/users/anonymous/products/recommend-segmented?placements=home_page.horizontal_recs1&fields=DEFAULT&storeId=016001&rrSessionId=&rcs="
-      )
-      .then(response => {
-        Vue.prototype.$receivedProducts = response.data.placements[0].products;
-        return (this.productList = response.data.placements[0].products);
-        // Vue.prototype.$receivedProducts = response.data[0].products;
-        // return (this.productList = response.data[0].products);
-      })
-      .catch(error => (this.error = error));
-  }
-}
+export default class HelloWorld extends Vue {}
 </script>
 
 <style scoped lang="less">
--- src/components/ProductMatrix.vue
+++ src/components/ProductMatrix.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="product-matrix">
-    <ProductCard id="7300156505033" />
-    <ProductCard id="2317301700002" />
+    <div v-if="error">{{ error }}</div>
+    <div v-for="product in productList" :key="product.code">
+      <ProductCard :id="product.code" />
+    </div>
   </div>
 </template>
 
@@ -9,9 +11,34 @@
 import Vue from "vue";
 import ProductCard from "@/components/ProductCard.vue";
 
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+
 export default Vue.extend({
   components: {
     ProductCard
+  },
+  data() {
+    return {
+      productList: Array,
+      error: ""
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        // "https://www.coop.se/api/hybris/ecommerce/product/recommendations?placements[]=generic_page.generic_recs1"
+        // "https://www.coop.se/api/hybris/ecommerce/product/recommendations?placements[]=home_page.horizontal_recs1"
+        "https://www.coop.se/ws/v2/coop/users/anonymous/products/recommend-segmented?placements=home_page.horizontal_recs1&fields=DEFAULT&storeId=016001&rrSessionId=&rcs="
+      )
+      .then(response => {
+        Vue.prototype.$receivedProducts = response.data.placements[0].products;
+        return (this.productList = response.data.placements[0].products);
+        // Vue.prototype.$receivedProducts = response.data[0].products;
+        // return (this.productList = response.data[0].products);
+      })
+      .catch(error => (this.error = error));
   }
 });
 </script>
