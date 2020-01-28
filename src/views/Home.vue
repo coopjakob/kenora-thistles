054422687390360bdc58577a690cdccdfa902bb1
Jakob Nanneson <jakob.nanneson@coop.se>
2020-01-28 14:18:25 +0100

    removed hello world

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,15 @@
 <template>
   <div class="home">
     <ProductMatrix />
-    <HelloWorld msg="Hello world string!" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import ProductMatrix from "@/components/ProductMatrix.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     ProductMatrix
   }
 };
