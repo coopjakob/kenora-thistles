47663f5b1a50e4cc5e67ac1e5fd51f52d843c8ad
Jakob Nanneson <jakob.nanneson@coop.se>
2020-03-11 10:51:49 +0100

    Buy button on mobile and new cart data #21

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -88,6 +88,12 @@
             class="add js-qty-selector-plus"
             aria-label="Öka antalet"
           ></button>
+          <button
+            class="buy-mobile js-qty-selector-plus"
+            aria-label="Lägg till i varukorgen"
+          >
+            Köp
+          </button>
         </div>
       </div>
     </div>
@@ -400,8 +406,22 @@ export default Vue.extend({
     button:disabled
       background-color: rgba(0,85,55,.3)
 
+    .buy-mobile
+      width: 100%
+      height: 34px
+      justify-content: center
+      align-items: center
+      background-color: #00aa46
+      border: 0
+      outline: 0
+      border-radius: 17px
+      cursor: pointer
+      text-align: center
+      font-size: 16px
+      color: white
+
     .add, .remove
-      display: flex
+      display: none
       width: 34px
       height: 34px
       background-color: #005537
@@ -426,6 +446,7 @@ export default Vue.extend({
       background-color: #001b11;
 
     input
+      display: none
       width: 34px
       height: 32px
       flex-grow: 1
@@ -446,23 +467,40 @@ export default Vue.extend({
       &::-ms-input-placeholder /* Microsoft Edge */
         color: white
 
-  .is-delayed .remove
-    cursor: wait!important
+    .is-delayed .remove
+      cursor: wait!important
 
-  .qty-selector.has-value
-    background-color: #f5f5f5
-    color: #00aa46
+    @media (min-width: 425px)
 
-    .add
-      background-color: white
-      background-image: url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjAiIHk9IjAiIHZpZXdCb3g9IjAgMCAxMSAxMSIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlPi5zdDB7ZmlsbDpub25lO3N0cm9rZTojMDBhYTQ2O3N0cm9rZS1taXRlcmxpbWl0OjEwfTwvc3R5bGU+PGcgaWQ9IktvbXBvbmVudGJpYmxpb3RlayI+PGcgaWQ9Il94NUJfS8OWUEtOQVBQX3g1RF8tX3gyRl8tU8O2ay1Db3B5IiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtMTUgLTE0KSI+PGcgaWQ9Il94NUJfSUtPTl94NURfLV94MkJfIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgxNSAxNCkiPjxwYXRoIGlkPSJMaW5lLTYiIGNsYXNzPSJzdDAiIGQ9Ik01LjUuNXYxMCIvPjxwYXRoIGlkPSJMaW5lLTZfMV8iIGNsYXNzPSJzdDAiIGQ9Ik0xMC41IDUuNUguNSIvPjwvZz48L2c+PC9nPjwvc3ZnPg==')
+      .add, .remove
+        display: flex
 
-    .remove
-      background-color: white
-      background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCI+PHBhdGggZD0iTTE5IDkuNUgxIiBmaWxsPSJub25lIiBzdHJva2U9IiMwMGFhNDYiLz48L3N2Zz4=')
+      input
+        display: block
 
-    input
+      .buy-mobile
+        display: none
+
+    &.has-value
+      background-color: #f5f5f5
       color: #00aa46
+
+      .buy-mobile
+        display: none
+
+      .add
+        display: flex
+        background-color: white
+        background-image: url('data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJMYXllcl8xIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHg9IjAiIHk9IjAiIHZpZXdCb3g9IjAgMCAxMSAxMSIgeG1sOnNwYWNlPSJwcmVzZXJ2ZSI+PHN0eWxlPi5zdDB7ZmlsbDpub25lO3N0cm9rZTojMDBhYTQ2O3N0cm9rZS1taXRlcmxpbWl0OjEwfTwvc3R5bGU+PGcgaWQ9IktvbXBvbmVudGJpYmxpb3RlayI+PGcgaWQ9Il94NUJfS8OWUEtOQVBQX3g1RF8tX3gyRl8tU8O2ay1Db3B5IiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgtMTUgLTE0KSI+PGcgaWQ9Il94NUJfSUtPTl94NURfLV94MkJfIiB0cmFuc2Zvcm09InRyYW5zbGF0ZSgxNSAxNCkiPjxwYXRoIGlkPSJMaW5lLTYiIGNsYXNzPSJzdDAiIGQ9Ik01LjUuNXYxMCIvPjxwYXRoIGlkPSJMaW5lLTZfMV8iIGNsYXNzPSJzdDAiIGQ9Ik0xMC41IDUuNUguNSIvPjwvZz48L2c+PC9nPjwvc3ZnPg==')
+
+      .remove
+        display: flex
+        background-color: white
+        background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyMCAyMCI+PHBhdGggZD0iTTE5IDkuNUgxIiBmaWxsPSJub25lIiBzdHJva2U9IiMwMGFhNDYiLz48L3N2Zz4=')
+
+      input
+        display: block
+        color: #00aa46
 
   input[type="number"]::-webkit-inner-spin-button, input[type="number"]::-webkit-outer-spin-button
     -webkit-appearance: none
--- src/store/index.ts
+++ src/store/index.ts
@@ -18,48 +18,52 @@ export default new Vuex.Store({
         type: "cartWsDTO",
         appliedOrderPromotions: [],
         appliedProductPromotions: [],
-        appliedVouchers: [],
-        calculated: true,
-        code: "30873951",
-        deliveryCost: {
-          currencyIso: "SEK",
-          formattedValue: "0:00 kr",
-          priceType: "TOTAL",
-          value: 0.0
-        },
-        deliveryItemsQuantity: 0,
-        deliveryMode: {
-          code: "homedelivery",
-          name: "Hemleverans"
-        },
-        deliveryOrderGroups: [],
-        entries: [],
+        code: "31026397",
+        entries: [
+          {
+            entryNumber: 0,
+            product: {
+              code: "7300170062642",
+              depositPrice: {
+                currencyIso: "SEK",
+                formattedValue: "0:-",
+                priceType: "BUY",
+                value: 0.0
+              },
+              keywords: [],
+              name: "Mellanmjölk Eko",
+              pickPrice: {
+                currencyIso: "SEK",
+                formattedValue: "11:50",
+                priceType: "BUY",
+                value: 11.5
+              },
+              unit: {
+                name: "st"
+              },
+              url:
+                "/handla/varor/mejeri-agg/mjolk-fil/mjolk/mellanmjolk-eko-7300170062642",
+              variances: []
+            },
+            quantity: 1,
+            replace: true,
+            totalPrice: {
+              currencyIso: "SEK",
+              formattedValue: "11:50",
+              priceType: "BUY",
+              value: 11.5
+            }
+          }
+        ],
         extraAmountToBeReserved: {
           currencyIso: "SEK",
           formattedValue: "4:00 kr",
           priceType: "TOTAL",
           value: 4.0
         },
-        guid: "a032e48d-f264-4dda-856f-f3db7741bd1e",
+        guid: "a6360054-821c-402d-bf85-3e87a4d6d8e5",
         isPartOfSubscription: false,
-        net: false,
-        orderDiscounts: {
-          currencyIso: "SEK",
-          formattedValue: "0:00 kr",
-          priceType: "TOTAL",
-          value: 0.0
-        },
-        pickupItemsQuantity: 0,
-        pickupOrderGroups: [],
         postCode: "17261",
-        site: "coop",
-        store: "016001",
-        subTotal: {
-          currencyIso: "SEK",
-          formattedValue: "0:00 kr",
-          priceType: "TOTAL",
-          value: 0.0
-        },
         timeWindow: {},
         totalDepositSum: {
           currencyIso: "SEK",
@@ -67,43 +71,42 @@ export default new Vuex.Store({
           priceType: "TOTAL",
           value: 0.0
         },
-        totalDiscounts: {
-          currencyIso: "SEK",
-          formattedValue: "0:00 kr",
-          priceType: "TOTAL",
-          value: 0.0
-        },
-        totalItems: 0,
+        totalItems: 1,
         totalPrice: {
           currencyIso: "SEK",
-          formattedValue: "0:00 kr",
+          formattedValue: "11:50 kr",
           priceType: "TOTAL",
-          value: 0.0
+          value: 11.5
         },
         totalPriceWithExtraAmountToReserve: {
           currencyIso: "SEK",
-          formattedValue: "4:00 kr",
+          formattedValue: "15:50 kr",
           priceType: "TOTAL",
-          value: 4.0
+          value: 15.5
         },
         totalPriceWithTax: {
           currencyIso: "SEK",
-          formattedValue: "0:00 kr",
+          formattedValue: "11:50 kr",
           priceType: "TOTAL",
-          value: 0.0
+          value: 11.5
         },
-        totalQuantity: 0,
-        totalTax: {
-          currencyIso: "SEK",
-          formattedValue: "0:00 kr",
-          priceType: "TOTAL",
-          value: 0.0
-        },
+        totalQuantity: 1,
+        totalTaxValues: [
+          {
+            appliedValue: {
+              currencyIso: "SEK",
+              formattedValue: "1:23 kr",
+              priceType: "TOTAL",
+              value: 1.23
+            },
+            value: 12.0
+          }
+        ],
         amountMissing: {
           currencyIso: "SEK",
-          formattedValue: "500:00 kr",
+          formattedValue: "488:50 kr",
           priceType: "TOTAL",
-          value: 500.0
+          value: 488.5
         },
         coopStore: {
           code: "016001",
@@ -111,21 +114,9 @@ export default new Vuex.Store({
           foodBagsOnly: false,
           name: "STOCKHOLM"
         },
-        hasSubscription: false,
         isExtraOrderCart: false,
-        minimumShoppingValue: {
-          currencyIso: "SEK",
-          formattedValue: "500:00 kr",
-          priceType: "TOTAL",
-          value: 500.0
-        },
-        minimumShoppingValueReached: false,
-        potentialOrderPromotions: [],
-        potentialProductPromotions: [],
         recipesGroups: [],
-        replaceAll: true,
-        subscriptionFrequency: 0,
-        totalUnitCount: 0
+        replaceAll: true
       }
     }
   },
