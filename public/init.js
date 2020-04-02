36c1c60dfeb39746c516d5589faf17b8291fe0e0
Jakob Nanneson <jakob.nanneson@coop.se>
2020-04-02 08:34:30 +0200

    update script with checks waits and isolation

--- public/init.js
+++ public/init.js
@@ -1,15 +1,29 @@
-replaceElement = document.querySelector(
-  "[data-type='MOST_PURCHASED_PRODUCTS']"
-);
-replaceElement.id = "app";
-replaceElement.innerHTML = "";
-replaceElement.className = "";
+(function() {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://coop-memory.netlify.com/css/app.css";
+  document.querySelector("head").appendChild(link);
 
-var script = document.createElement("script");
-script.src = "https://coop-memory.netlify.com/js/app.js";
-document.querySelector("head").appendChild(script);
+  var checkExist = setInterval(function() {
+    if (window.ACC) {
+      clearInterval(checkExist);
+      console.log("found config");
 
-var link = document.createElement("link");
-link.rel = "stylesheet";
-link.href = "https://coop-memory.netlify.com/css/app.css";
-document.querySelector("head").appendChild(link);
+      var replaceElement = document.querySelector(
+        "[data-type='MOST_PURCHASED_PRODUCTS']"
+      );
+
+      if (replaceElement) {
+        replaceElement.id = "app";
+        replaceElement.innerHTML = "";
+        replaceElement.className = "";
+
+        var script = document.createElement("script");
+        script.src = "https://coop-memory.netlify.com/js/app.js";
+        document.querySelector("head").appendChild(script);
+      }
+    } else {
+      console.log("no config");
+    }
+  }, 100);
+})();
